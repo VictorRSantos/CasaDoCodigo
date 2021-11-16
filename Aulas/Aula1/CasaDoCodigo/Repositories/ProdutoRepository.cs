77d6aab2d8d7c33536e754225193fc289a346edd
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2021-11-15 22:37:19 -0300

    Add project files.

---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -0,0 +1,57 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class ProdutoRepository : BaseRepository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(ApplicationContext context) : base(context)
+        {
+        }
+
+        public IList<Produto> GetProdutos()
+        {
+            return dbSet.ToList();
+        }
+
+        public void SaveProdutos(List<Livro> livros)
+        {
+            foreach (var livro in livros)
+            {
+                if (!dbSet.Where(p => p.Codigo == livro.Codigo).Any())
+                {
+                    dbSet.Add(new Produto(livro.Codigo, livro.Nome, livro.Preco));
+                }
+
+
+
+            }
+
+            context.SaveChanges();
+        }
+
+
+        public static List<Livro> GetLivros()
+        {
+            var json = File.ReadAllText("livros.json");
+
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            return livros;
+        }
+
+      
+        public class Livro
+        {
+
+            public string Codigo { get; set; }
+            public string Nome { get; set; }
+            public decimal Preco { get; set; }
+
+        }
+
+    }
+}
